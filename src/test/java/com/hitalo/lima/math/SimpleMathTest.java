42f49689acb7fbe737f2c80dc5ddbeb6da48b258
hitalo-lima <hitalolima173@gmail.com>
2026-02-21 17:46:05 -0300

    refactor: enhance test method names and add display names for clarity in SimpleMathTest

--- src/test/java/com/hitalo/lima/math/SimpleMathTest.java
+++ src/test/java/com/hitalo/lima/math/SimpleMathTest.java
@@ -2,11 +2,16 @@ package com.hitalo.lima.math;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Test math operations in SimpleMath Class")
 public class SimpleMathTest {
+    // test[System Under Test]_[Condition or State Change]_[Expected Result]
+
     @Test
-    void testSum() {
+    @DisplayName("Test 6.2 + 2 = 8.2")
+    void testSum_When_SixDotTwoAddedByTwo_ShouldReturnEightDotTwo() {
         SimpleMath math = new SimpleMath();
 
         Double firstNumber = 6.2D;
@@ -15,6 +20,81 @@ public class SimpleMathTest {
         Double actual = math.sum(firstNumber, secondNumber);
 
         assertEquals(expected, actual, () -> firstNumber + " + " + secondNumber + " didn't produced " + expected);
-        assertNotNull(actual);
+    }
+
+    @Test
+    @DisplayName("Test 6.2 - 2 = 4.2")
+
+    void testSubtraction() {
+        SimpleMath math = new SimpleMath();
+
+        Double firstNumber = 6.2D;
+        Double secondNumber = 2D;
+        Double expected = 4.2D;
+        Double actual = math.subtraction(firstNumber, secondNumber);
+
+        assertEquals(expected, actual, () -> firstNumber + " - " + secondNumber + " didn't produced " + expected);
+    }
+
+    @Test
+    @DisplayName("Test 6.2 * 2 = 12.4")
+
+    void testMultiplication() {
+        SimpleMath math = new SimpleMath();
+
+        Double firstNumber = 6.2D;
+        Double secondNumber = 2D;
+        Double expected = 12.4D;
+        Double actual = math.multiplication(firstNumber, secondNumber);
+
+        assertEquals(expected, actual, () -> firstNumber + " * " + secondNumber + " didn't produced " + expected);
+    }
+
+    @Test
+    @DisplayName("Test 6.2 / 2 = 3.1")
+
+    void testDivision() {
+        SimpleMath math = new SimpleMath();
+
+        Double firstNumber = 6.2D;
+        Double secondNumber = 2D;
+        Double expected = 3.1D;
+        Double actual = math.division(firstNumber, secondNumber);
+
+        assertEquals(expected, actual, () -> firstNumber + " / " + secondNumber + " didn't produced " + expected);
+    }
+
+    @Test
+    @DisplayName("Test (6.2 + 2)/2 = 4.1")
+
+    void testMean() {
+        SimpleMath math = new SimpleMath();
+
+        Double firstNumber = 6.2D;
+        Double secondNumber = 2D;
+        Double expected = 4.1D;
+        Double actual = math.mean(firstNumber, secondNumber);
+
+        assertEquals(expected, actual,
+                () -> "(" + firstNumber + " + " + secondNumber + ")/2 didn't produced " + expected);
+    }
+
+    @Test
+    @DisplayName("Test sqrt(9) = 3")
+    void testSquareRoot() {
+        SimpleMath math = new SimpleMath();
+
+        Double number = 9D;
+        Double expected = 3D;
+        Double actual = math.squareRoot(number);
+
+        assertEquals(expected, actual, () -> "sqrt(" + number + ") didn't produced " + expected);
+    }
+
+    @Test
+    @DisplayName("Test division by zero")
+
+    void testDivision_When_FirstNumberIdDividedByZero_ShouldThrowArithmeticException() {
+        fail();
     }
 }
